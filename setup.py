9bc67cfb16a61609f1d3c6ae162fa8021392e56b
xikasan <xika0907@gmail.com>
2020-01-12 00:24:08 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='xaircraft',
+    version='0.1.0',
+    description="aircraft model and env set",
+    long_description=readme,
+    author='xikasan',
+    # author_email='',
+    url='https://github.com/xikasan/xaircraft',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
