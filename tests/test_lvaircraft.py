1ff51b0c1f7a003dd7bc8e377538d9d45fd8830a
xikasan <xika0907@gmail.com>
2020-03-05 22:15:59 +0900

    test extended model LVAircraftEx

--- tests/test_lvaircraft.py
+++ tests/test_lvaircraft.py
@@ -1,16 +1,48 @@
 # coding: utf-8
 
 import numpy as np
+import pandas as pd
 import xtools as xt
-from xaircraft.models.lvaircraft import LVAircraft
+import xtools.simulation as xsim
+from xaircraft.models.lvaircraft import LVAircraftEx
+from matplotlib import pyplot as plt
 
 
 def test_LVAircraft():
     xt.info("test for LVAircraft")
 
     dt = 0.05
-    lvair = LVAircraft(dt)
-    lvair(np.ones(2))
+    env = LVAircraftEx(dt, range_elevator=xt.d2r([-3, 3]))
+    obs = env.reset()
+
+    due = 300
+
+    Ht = 12500
+    Kp = -0.001
+
+    rb = xsim.ReplayBuffer({
+        "time": 1,
+        "tgt": 1,
+        "obs": 6
+    }, capacity=int(due / dt + 1))
+
+    rb.add(time=0, tgt=Ht, obs=obs)
+    for time in xsim.generate_step_time(due, dt):
+        H = obs[env.IX_H]
+        error = Ht - H
+        act = error * Kp
+        act = np.array([0, act]).astype(env.dtype)
+        obs = env(act)
+        rb.add(time=time, tgt=Ht, obs=obs)
+
+    result = xsim.Retriever(rb.buffer())
+    result = pd.DataFrame({
+        "time": result("time"),
+        "target": result("tgt"),
+        "altitude": result("obs", idx=env.IX_H)
+    })
+    result.plot(x="time")
+    plt.show()
 
 
 if __name__ == '__main__':
