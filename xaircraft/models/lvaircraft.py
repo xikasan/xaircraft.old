ba454945ac5e08a343b16ba5351a61dcf99a488d
xikasan <xika0907@gmail.com>
2020-03-05 22:15:50 +0900

    dev extended model LVAircraftEx

--- xaircraft/models/lvaircraft.py
+++ xaircraft/models/lvaircraft.py
@@ -11,6 +11,14 @@ class LVAircraftEx(xs.BaseModel):
     H0 = 12192  # m
     U0 = 625    # m/s
 
+    # indices
+    IX_U = 0
+    IX_H = 1
+    IX_u = 2
+    IX_w = 3
+    IX_T = 4
+    IX_q = 5
+
     def __init__(
             self,
             dt,
@@ -22,13 +30,60 @@ class LVAircraftEx(xs.BaseModel):
         super().__init__(dt, dtype=dtype, name=name)
 
         # parameters
-        self.A, self.B = self.construct_matrices()
+        self._A, self._B = self.construct_matrices()
+        self._x = np.zeros(6, dtype=self.dtype)
+
+        # env params
+        # action space
+        # U = [throttle, elevator]
+        self._act_low  = np.array([np.min(range_throttle), np.min(range_elevator)])
+        self._act_high = np.array([np.max(range_throttle), np.max(range_elevator)])
+        # observation space
+        self._obs_low, self._obs_high, _ = self.generate_inf_space(6)
+
+    def __call__(self, action):
+        action = np.clip(action, self._act_low, self._act_high)
+        print(action)
+        fn = lambda x: x.dot(self._A) + action.dot(self._B)
+        dx = xs.no_time_rungekutta(fn, self.dt, self._x)
+        self._x += dx * self.dt
+        return self.get_observation()
+
+    def reset(self):
+        self._x = np.zeros(6, dtype=self.dtype)
+        self._x[self.IX_U] = self.U0
+        self._x[self.IX_H] = self.H0
+        return self.get_observation()
+
+    def get_observation(self):
+        return self._x
+
+    def get_state(self):
+        return self._x[self.IX_u:]
+
+    def get_H(self):
+        return self._x[self.IX_H]
+
+    def get_U(self):
+        return self._x[self.IX_U]
+
+    def get_u(self):
+        return self._x[self.IX_u]
+
+    def get_w(self):
+        return self._x[self.IX_w]
+
+    def get_T(self):
+        return self._x[self.IX_T]
+
+    def get_q(self):
+        return self._x[self.IX_q]
 
     def construct_matrices(self):
         A = np.array([
             #H  U   u        w         Theta     q
-            [0, 0,  0,       1,        0,        0],       # H
-            [0, 0,  1,       0,        0,        0],       # U
+            [0, 0,  1,       0,        0,        0],       # H
+            [0, 0,  0,       1,        0,        0],       # U
             [0, 0, -0.0225,  0.0022, -32.3819,   0],       # u
             [0, 0, -0.2282, -0.4038,   0,      869],       # w
             [0, 0,  0,       0,        0,        1],       # Theta
@@ -69,7 +124,7 @@ class LVAircraft(xs.BaseModel):
         self._act_low  = np.array([np.min(range_throttle), np.min(range_elevator)])
         self._act_high = np.array([np.max(range_throttle), np.max(range_elevator)])
         # observation space
-        self._obs_low, self._obs_high = self.generate_inf_range(4)
+        self._obs_low, self._obs_high, _ = self.generate_inf_space(4)
 
     def __call__(self, action):
         action = np.clip(action, self._act_low, self._act_high)
