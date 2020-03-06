d41470d87d875db34656d3470ffff0be04797354
xikasan <xika0907@gmail.com>
2020-03-06 12:30:44 +0900

    update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='xaircraft',
-    version='0.1.0',
+    version='0.2.0',
     description="aircraft model and env set",
     long_description=readme,
     author='xikasan',
